02013e6394a979e3268539a045e8f77c78a7fd02
榆讓 張 <108111101@mail.aeust.edu.tw>
2021-10-01 15:37:58 +0800

    華氏攝氏轉換

--- 110-1Practice2-1/Trans.aspx.cs
+++ 110-1Practice2-1/Trans.aspx.cs
@@ -11,13 +11,10 @@ namespace _110_1Practice2_1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            double d_Value = 66;  //設定一個 d_Value 變數為66
-
-            double d_Value2 = 0;  //設定一個 d_Value2 變數來存放運算結果
-
-            d_Value2 = 5.0 * (d_Value - 32.0) / 9.0; //華氏轉攝氏
-
-            Response.Write("d_Value " + "= 66F " + " =  " + d_Value2 + " 度"); //印出結果
+            double d_Value = 66;
+            double d_Value2 = 0;
+            d_Value2 = 5.0 * (d_Value - 32.0) / 9.0;
+            Response.Write("d_Value" + "=66F" + "=解答" + d_Value2);
 
         }
     }
