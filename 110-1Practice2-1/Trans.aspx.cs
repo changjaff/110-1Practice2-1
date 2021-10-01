b22b440484f65fa99dab7bedf0da47f13e6d4476
榆讓 張 <108111101@mail.aeust.edu.tw>
2021-10-01 12:42:18 +0800

    華氏攝氏轉換

--- 110-1Practice2-1/Trans.aspx.cs
+++ 110-1Practice2-1/Trans.aspx.cs
@@ -17,7 +17,7 @@ namespace _110_1Practice2_1
 
             d_Value2 = 5.0 * (d_Value - 32.0) / 9.0; //華氏轉攝氏
 
-            Response.Write("d_Value " + "= 66F " + " =  " + d_Value2 + ' 度'); //印出結果
+            Response.Write("d_Value " + "= 66F " + " =  " + d_Value2 + " 度"); //印出結果
 
         }
     }
